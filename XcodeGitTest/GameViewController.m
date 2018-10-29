8198e0eb4bcd065014b4c3072640767c6795e9d6
Pikdays <byt.quan@gmail.com>
2018-10-29 21:30:09 +0800

    GameVC_RemoveComment

--- XcodeGitTest/GameViewController.m
+++ XcodeGitTest/GameViewController.m
@@ -14,15 +14,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Load the SKScene from 'GameScene.sks'
     GameScene *scene = (GameScene *)[SKScene nodeWithFileNamed:@"GameScene"];
-    
-    // Set the scale mode to scale to fit the window
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     SKView *skView = (SKView *)self.view;
-    
-    // Present the scene
     [skView presentScene:scene];
     
     skView.showsFPS = YES;
